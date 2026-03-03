7aa66c1275f7ce7ea8ee542563f1a169c4eb21d1
muel-ci <muel-ci@localhost>
2026-03-03 20:33:02 +0900

    feat: EmbeddedApp에 Discord Embedded App SDK 초기화 추가

--- src/pages/EmbeddedApp.tsx
+++ src/pages/EmbeddedApp.tsx
@@ -1,6 +1,12 @@
-﻿import { ResearchPageLayout } from '../components/sections/ResearchPageLayout';
+﻿
+import { useEffect } from 'react';
+import * as DiscordSDK from '@discord/embedded-app-sdk';
 
 export const EmbeddedApp = () => {
+  useEffect(() => {
+    DiscordSDK.ready();
+    // DiscordSDK.getUser().then(console.log); // 예시: 유저 정보 가져오기
+  }, []);
   return <ResearchPageLayout presetKey="embedded" />;
 };
 
